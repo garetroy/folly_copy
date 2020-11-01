e040a570d72133286768f8e8d5bd872f0bbcd89e
Garett Roberts <mygarett@gmail.com>
2020-10-31 18:02:34 -0700

    Fixed build issues for Math.h

--- folly/Math.h
+++ folly/Math.h
@@ -18,6 +18,7 @@
 
 #include <stdint.h>
 #include <cmath>
+#include <limits>
 #include <type_traits>
 
 namespace folly {
@@ -69,13 +70,11 @@ inline constexpr T divRoundAwayBranchful(T num, T denom)
             (std::is_signed<T>::value && (num ^ denom) < 0 ? -1 : 1);
 }
 
-//GNOTE: What?
 template <typename N, typename D>
 using IdivResultType = typename std::enable_if<
     std::is_integral<N>::value && std::is_integral<D>::value &&
     !std::is_same<N, bool>::value && !std::is_same<D, bool>::value,
-    decltype(N{1} / D{1}>::type;
-
+    decltype(N{1} / D{1})>::type;
 } // namespace detail
 
 #if defined(__arm__) && !FOLLY_AARCH64
@@ -98,8 +97,9 @@ inline constexpr detail::IdivResultType<N, D> divCeil(N num, D denom) {
     using R = decltype(num / denom);
     return detail::IdivResultType<N, D>(
         kIntegerDivisionGivesRemainder && std::is_signed<R>::value
-            ? detail::divCielBranchless<R>(num, denom)
+            ? detail::divCeilBranchless<R>(num, denom)
             : detail::divCeilBranchful<R>(num, denom));
+}
 
 template<typename N, typename D>
 inline constexpr detail::IdivResultType<N, D> divRoundAway(N num, D denom) {
@@ -108,6 +108,7 @@ inline constexpr detail::IdivResultType<N, D> divRoundAway(N num, D denom) {
         kIntegerDivisionGivesRemainder && std::is_signed<R>::value
             ? detail::divRoundAwayBranchless<R>(num, denom)
             : detail::divRoundAwayBranchful<R>(num, denom));
+}
 
 template <class _Tp>
 constexpr std::enable_if_t<
@@ -125,7 +126,7 @@ midpoint(_Tp __a, _Tp __b) noexcept {
 
     _Up __half_diff = (__diff / 2) + (__sign_bit << __bitshift) + (__sign_bit & __diff);
     
-    return __a + __ half_diff;
+    return __a + __half_diff;
 }
 
 template <class _TPtr>
@@ -133,11 +134,11 @@ constexpr std::enable_if_t<
     std::is_pointer<_TPtr>::value &&
         std::is_object<std::remove_pointer_t<_TPtr>>::value &&
         //GNOTE: This is getting the value the pointer is pointing at and checking if object and not void
-        !std::is_void<std::remove_pointer_t<_TPtr>::value &&
+        !std::is_void<std::remove_pointer_t<_TPtr>>::value &&
         (sizeof(std::remove_pointer_t<_TPtr>) > 0),
     _TPtr>
 midpoint(_TPtr __a, _TPtr __b) noexcept {
-    return __a + midpoint(std::ptrdiff_t(0), __b - __ a);
+    return __a + midpoint(std::ptrdiff_t(0), __b - __a);
 }
 
 template <class _Fp>
@@ -148,7 +149,7 @@ constexpr std::enable_if_t<std::is_floating_point<_Fp>::value, _Fp> midpoint(
     constexpr _Fp __hi = std::numeric_limits<_Fp>::max()/2;
     return std::abs(__a) <= __hi && std::abs(__b) <= __hi ?
         (__a + __b)/2 
-        : std::abs(__a) < __lo ? __a + b/2
+        : std::abs(__a) < __lo ? __a + __b/2
         : std::abs(__b) < __lo ? __a/2 + __b :
         __a/2 + __b/2;
 }
